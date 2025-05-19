6ad1087bf149d427435779346c9ac4d72cfa0dd0
saisijju <saisijju_reddygarlapati@epam.com>
2025-05-19 22:27:49 +0530

    parallel execution with allure and extent reports

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,15 +2,21 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-    features = "src/test/resources/features",
-    glue = {"stepDefinitions","hooks"},
-    plugin = {"pretty",
-            "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-            "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-            "html:target/cucumber-html-report" },
-    monochrome = true
+        features = "src/test/resources/features",
+        glue = {"stepDefinitions","hooks"},
+        plugin = {"pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "html:target/cucumber-html-report" },
+        monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
